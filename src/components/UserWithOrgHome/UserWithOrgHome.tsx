2aaac469905cb50efda7f2dd8fe869b526fcb8ec
Liel Almog <liel.almogg@gmail.com>
2025-05-18 19:59:31 +0300

    Feat: Finish the user with org home page

--- src/components/UserWithOrgHome/UserWithOrgHome.tsx
+++ src/components/UserWithOrgHome/UserWithOrgHome.tsx
@@ -1,9 +1,10 @@
 import { TokenUserWithOrgSchema } from "@/models/auth.model";
 import { CommonProcessesDashboard } from "../dashboard/CommonProcessesDashboard";
+import { ProcessesWithMostRulesDashboard } from "../dashboard/ProcessWithMostRulesDashboard";
 import { RulesByChainDashboard } from "../dashboard/RulesByChainDashboard";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 import { UserWithOrgHomeHeader } from "./UserWithOrgHomeHeader";
+import { Separator } from "../ui/separator";
 
 export interface UserWithOrgHomeProps {
   user: TokenUserWithOrgSchema;
@@ -11,34 +12,34 @@ export interface UserWithOrgHomeProps {
 
 export const UserWithOrgHome = ({ user }: UserWithOrgHomeProps) => {
   return (
-    <main className="flex flex-col size-full bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 overflow-auto">
+    <main className="flex flex-col size-full overflow-auto">
       <UserWithOrgHomeHeader />
-      <div className="flex flex-col items-center justify-center size-full bg-background">
-        <Card className="max-w-3xl shadow-lg">
-          <CardHeader>
-            <CardTitle className="text-2xl font-semibold text-center">
-              Welcome {user.name}!
-            </CardTitle>
-            <CardDescription className="text-center text-md pt-2">
+      <ScrollArea className=" w-full overflow-auto">
+        <div className="container mx-auto">
+          <div className="p-6">
+            <h1 className="text-2xl font-semibold text-center">Welcome {user.name}!</h1>
+            <p className="text-center text-md pt-2 text-gray-600 dark:text-gray-300">
               You are currently part of the organization.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <h2 className="text-lg font-medium text-white mb-4 ">
+            </p>
+          </div>
+
+          <Separator className="my-4" />
+
+          <div className="p-6 max-w-4xl mx-auto">
+            <h2 className="text-lg font-medium text-white mb-4">
               Those are your organization dashboards
             </h2>
 
-            <ScrollArea className="h-[500px] w-full rounded-md border overflow-auto">
-              <div className="p-4">
-                <RulesByChainDashboard />
-                <CommonProcessesDashboard />
-              </div>
+            <div className="p-4 flex flex-col gap-4">
+              <RulesByChainDashboard />
+              <CommonProcessesDashboard />
+              <ProcessesWithMostRulesDashboard />
+            </div>
 
-              <ScrollBar orientation="vertical" />
-            </ScrollArea>
-          </CardContent>
-        </Card>
-      </div>
+            <ScrollBar orientation="vertical" />
+          </div>
+        </div>
+      </ScrollArea>
     </main>
   );
 };
